fixed a minus sign bug, which changed the value of sideslip and corresponding moments

examples/TUDELFT_V3_KITE/tutorial.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import numpy as np
 from VSM.core.BodyAerodynamics import BodyAerodynamics
 from VSM.core.Solver import Solver
 from VSM.plotting import (
@@ -50,7 +51,7 @@ def main():
     ### 2. defining settings
     n_panels = 50
     spanwise_panel_distribution = "uniform"
-    solver_base_version = Solver()
+    solver_base_version = Solver(reference_point=np.array([0.0, 0.0, 0.0]))
 
     # Step 1: Instantiate BodyAerodynamics objects from different YAML configs
     cad_derived_geometry_dir = (

src/VSM/core/BodyAerodynamics.py
@@ -1329,7 +1329,7 @@ class BodyAerodynamics:
         Parameters:
         Umag (float): Magnitude of the velocity.
         angle_of_attack (float): Angle of attack in degrees.
-        side_slip (float): Sideslip angle in degrees, a minus is added because its defined counter-clockwise.
+        side_slip (float): Sideslip angle in degrees.
         yaw_rate (float): Yaw rate about the body z-axis, default is 0.0.
         pitch_rate (float): Pitch rate about the body y-axis, default is 0.0.
         roll_rate (float): Roll rate about the body x-axis, default is 0.0.
@@ -1337,8 +1337,7 @@ class BodyAerodynamics:
         """
         # Convert angles to radians
         aoa_rad = np.deg2rad(angle_of_attack)
-        # a (-) is added because its defined counter-clockwise
-        side_slip_rad = -np.deg2rad(side_slip)
+        side_slip_rad = np.deg2rad(side_slip)
 
         # Set the va attribute using the setter with keyword arguments
         # The va setter requires keyword-only arguments after the va parameter