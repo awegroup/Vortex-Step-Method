7c7a7d6b1643db7ea8173e76e77e32262b21ec61
ocayon <oriol.cayon@gmail.com>
2026-04-16 21:42:29 +0200

    Add compute_quasi_steady_fast_timescales and linearize_fast_dynamics_from_trim_jacobian imports to __init__.py

--- src/VSM/__init__.py
+++ src/VSM/__init__.py
@@ -1,5 +1,7 @@
 from VSM.quasi_steady_state import (
     AxisDefinition,
     DEFAULT_AXES,
+    compute_quasi_steady_fast_timescales,
+    linearize_fast_dynamics_from_trim_jacobian,
     solve_quasi_steady_state,
 )
